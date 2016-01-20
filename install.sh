33077853858f8d460543f19b0f8a3ef2dfc46ad2
MickaelBlet <mblet@student.42.fr>
2016-01-20 13:09:38 +0100

    refresh git

--- install.sh
+++ install.sh
@@ -9,6 +9,7 @@ function print_menu()
 {
 	local index=$1
 
+	printf "\n\r$index"
 	printf "\n\r"
 	if [[ $index == "1" ]]; then
 		printf "\033[7m"
@@ -49,27 +50,40 @@ printf "%*s%*s\n\r" $cols1 "MBLET CONFIGS" $cols2 ""
 printf "\033[38;5;226m\033[7m"
 printf "%*s%*s" $cols1 "INSTALL SPEED" $cols2 ""
 printf "\033[0m\n\r"
-printf "\n\r1. ZSH + PROMPT + VIM\n\r2. ZSH + PROMPT\n\r3. PROMPT\n\r4. VIM\n\r\n\rChoose your option : "
-read -r -n 1 C
+
+index=0
+
+print_menu $index
+
+while read -r -n 1 C; do
 case $C in
-	1) printf "\r\033[K\n";;
-	2) echo "PROMPT";;
-	3) echo "ZSH";;
-	4) echo "VIM";;
 	$'\x1b')
 		read -srn1 -t 1 D
 		if [[ "$D" == "[" ]]; then
 			read -rsn1 -t 1 E
 			case $E in
-				A) print_menu 1;;
-				B) printf "Down\n";;
-				C) printf "Right\n";;
-				D) printf "Left\n";;
+				A) index=$(($index - 1));;
+				B) index=$(($index + 1));;
 			esac
+			if [[ $index > "4" ]]; then
+				index=4
+			fi
+			if [[ $index < "0" ]]; then
+				index=0
+			fi
+			printf "\r\033[K"
+
+			tput up
+			tput up
+			tput up
+			tput up
+			tput up
+			tput up
+			print_menu $index
 		fi;;
-	$'') echo enter;;
-	*) echo test;;
+	*) break
 esac
+done
 #tput up
 #tput up
 #tput up
