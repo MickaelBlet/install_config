1949a4e4dbef255c18b6301f154c6e041c933817
mblet <mblet@mblets-Mac.local>
2016-03-11 00:11:41 -0800

    2016-03-11 00:11:37 test

--- .install/config/prompt.zsh
+++ .install/config/prompt.zsh
@@ -5,6 +5,8 @@ autoload -U colors && colors
 rm -fr ~/Library/Fonts/Meslo\ LG\ L\ DZ\ Regular\ for\ Powerline.otf
 cp ~/#_my_config/Fonts/Meslo\ LG\ L\ DZ\ Regular\ for\ Powerline.otf ~/Library/Fonts/
 
+export PROMPT_GIT="true"
+
 TMOUT=60
 TRAPALRM() {
 	if [[ ${commands[(r)$LBUFFER]} == $LBUFFER ]] ; then
