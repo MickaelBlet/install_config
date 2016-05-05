b26551bed26cde79c6c3e6169c72cd8946937b73
Mickael BLET <mblet@e3r2p22.42.fr>
2016-05-05 14:42:49 +0200

    2016-05-05 14:42:44 it's good

--- install.sh
+++ install.sh
@@ -3,12 +3,12 @@
 ##
 #    install.sh                                         :+:      :+:    :+:    #
 ##
- 
+
 menu=(	"ZSH + PROMPT + VIM"\
-		"ZSH + PROMPT"\
-		"PROMPT"\
-		"VIM"\
-		"SAVE CURRENT CONFIG")
+	"ZSH + PROMPT"\
+	"PROMPT"\
+	"VIM"\
+	"SAVE CURRENT CONFIG")
 
 menulength=${#menu[@]}
 menulengthend=`echo "${menulength}-1"|bc`
@@ -78,25 +78,13 @@ function menu4()
 ##
 
 clear
-cols1=`echo "$(tput cols)/2+14/2"|bc`
-cols2=`echo "$(tput cols)/2-14/2"|bc`
-result=`echo "$cols1+$cols2"|bc`
-
-if [ $result != $(tput cols) ]; then
-	cols1=`echo "$cols1+1"|bc`
-fi
-
-printf "\033[48;5;244m\033[38;5;231m"
-printf "%*s%*s\n\r" $cols1 "MBLET CONFIGS" $cols2 ""
-printf "\033[38;5;226m\033[7m"
-printf "%*s%*s" $cols1 "INSTALL SPEED" $cols2 ""
-printf "\033[0m\n\r"
 
 tput civis # hide cursor
 
 trap ctrl_c INT
 
-function ctrl_c() {
+function ctrl_c()
+{
 	tput cnorm
 	exit
 }
@@ -124,6 +112,23 @@ function print_menu()
 {
 	local index=$1
 
+	tput el1
+	tput cup 0 0
+
+	cols1=`echo "$(tput cols)/2+14/2"|bc`
+	cols2=`echo "$(tput cols)/2-14/2"|bc`
+	result=`echo "$cols1+$cols2"|bc`
+
+	if [ $result != $(tput cols) ]; then
+		cols1=`echo "$cols1+1"|bc`
+	fi
+
+	printf "\033[48;5;244m\033[38;5;231m"
+	printf "%*s%*s\n\r" $cols1 "MBLET CONFIGS" $cols2 ""
+	printf "\033[38;5;226m\033[7m"
+	printf "%*s%*s" $cols1 "INSTALL SPEED" $cols2 ""
+	printf "\033[0m\n\r\033[K"
+
 	printf "\n\r"
 	for (( i=0; i<${menulength}; i++ ));
 	do
@@ -133,46 +138,42 @@ function print_menu()
 			printf "\033[0m"
 		fi
 		print_middle "${menu[$i]}"
-		printf "\033[0m"
+		printf "\033[0m\033[K"
 	done
+	printf "\033[0m\n\r\033[K"
+	tput ed
 }
 
 ##
 ## while
 ##
 
- index=0
+index=0
 
 print_menu $index
 
 while read -r -n 1 C; do
-case $C in
-	$'\x1b')
-		read -srn1 -t 1 D
-		if [[ "$D" == "[" ]]; then
-			read -rsn1 -t 1 E
-			case $E in
-				A) index=$(($index - 1));;
-				B) index=$(($index + 1));;
-				C) index=$(($index + 1));;
-				D) index=$(($index - 1));;
-			esac
-			if [[ $index > "${menulengthend}" ]]; then
-				index=${menulengthend}
-			fi
-			if [[ $index < "0" ]]; then
-				index=0
-			fi
-			printf "\r\033[K"
-			for i in "${menu[@]}"
-			do
-				tput up
-			done
-			tput up
-			print_menu $index
-		fi;;
-	*)	break;;
-esac
+	case $C in
+		$'\x1b')
+			read -srn1 -t 1 D
+			if [[ "$D" == "[" ]]; then
+				read -rsn1 -t 1 E
+				case $E in
+					A) index=$(($index - 1));;
+					B) index=$(($index + 1));;
+					C) index=$(($index + 1));;
+					D) index=$(($index - 1));;
+				esac
+				if [[ $index > "${menulengthend}" ]]; then
+					index=${menulengthend}
+				fi
+				if [[ $index < "0" ]]; then
+					index=0
+				fi
+				print_menu $index
+			fi;;
+		*)	break;;
+	esac
 done
 
 tput cnorm
